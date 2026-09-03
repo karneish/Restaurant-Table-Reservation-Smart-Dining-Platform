0ebc29a0499338d5358959d4235d23afa4325c2c
karneish <karneish.sk@gmail.com>
2026-09-03 22:00:32 +0530

    feat(backend): add AppConstants class with application-wide constants

--- services/common/src/main/java/com/ticketbooking/common/constants/AppConstants.java
+++ services/common/src/main/java/com/ticketbooking/common/constants/AppConstants.java
@@ -1,10 +1,17 @@
-package com.ticketbooking.common.constants;
-
-public class AppConstants {
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_HEADER = "Authorization";
-    public static final String SERVICE_REGISTRY_URL = "http://localhost:8761/eureka/";
-    public static final String DEFAULT_PAGE_NUMBER = "0";
-    public static final String DEFAULT_PAGE_SIZE = "10";
-    public static final String DEFAULT_SORT_DIRECTION = "desc";
+﻿package com.ticketbooking.common.constants;
+public final class AppConstants {
+    private AppConstants() {}
+    public static final String GUEST_EMAIL = "guest@tablehub.com";
+    public static final String HEADER_USER_EMAIL = "X-User-Email";
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final int HOLD_EXPIRY_MINUTES = 15;
+    public static final int PARTY_SIZE_MIN = 1;
+    public static final int PARTY_SIZE_MAX = 20;
+    public static final int RATING_MIN = 1;
+    public static final int RATING_MAX = 5;
+    public static final int OTP_LENGTH = 6;
+    public static final int OTP_EXPIRY_MINUTES = 10;
+    public static final int JWT_ACCESS_EXPIRY_MINUTES = 30;
+    public static final int JWT_REFRESH_EXPIRY_DAYS = 7;
 }
