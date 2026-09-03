dea5171f7efed998231c65de429e5811dbc488af
karneish <karneish.sk@gmail.com>
2026-09-03 22:05:10 +0530

    feat(backend): add CleaningStatus enum for table cleaning states

---
+++ services/common/src/main/java/com/ticketbooking/common/enums/CleaningStatus.java
@@ -0,0 +1,7 @@
+﻿package com.ticketbooking.common.enums;
+public enum CleaningStatus {
+    READY, DIRTY, CLEANING;
+    public boolean canTransitionTo(CleaningStatus next) {
+        return switch (this) { case READY -> next == DIRTY; case DIRTY -> next == CLEANING; case CLEANING -> next == READY; };
+    }
+}
