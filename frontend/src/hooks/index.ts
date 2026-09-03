69de88b2bd5e84e248e8b55dc9f7ef75777bc263
karneish <karneish.sk@gmail.com>
2026-09-03 22:00:20 +0530

    feat(frontend): add barrel export index for all custom hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,4 @@
+﻿export { useDebounce } from './useDebounce';
+export { useLocalStorage } from './useLocalStorage';
+export { useClickOutside } from './useClickOutside';
+export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery';
