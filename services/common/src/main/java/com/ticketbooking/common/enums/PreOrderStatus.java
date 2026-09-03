13b282c378bd796f010734c79f2f512237d2289d
karneish <karneish.sk@gmail.com>
2026-09-03 22:05:13 +0530

    feat(backend): add PreOrderStatus enum for order lifecycle

---
+++ services/common/src/main/java/com/ticketbooking/common/enums/PreOrderStatus.java
@@ -0,0 +1,7 @@
+﻿package com.ticketbooking.common.enums;
+public enum PreOrderStatus {
+    DRAFT, PLACED, IN_PREP, SERVED;
+    public boolean canTransitionTo(PreOrderStatus next) {
+        return switch (this) { case DRAFT -> next == PLACED; case PLACED -> next == IN_PREP; case IN_PREP -> next == SERVED; case SERVED -> false; };
+    }
+}
