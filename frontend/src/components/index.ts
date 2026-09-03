1f9cd34b1916033538290db1584ae74cba9d22bb
karneish <karneish.sk@gmail.com>
2026-09-03 22:00:28 +0530

    feat(frontend): add barrel export index for all components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+﻿export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as StatusBadge } from './StatusBadge';
+export { default as StarRating } from './StarRating';
+export { default as EmptyState } from './EmptyState';
+export { default as ConfirmDialog } from './ConfirmDialog';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as NotificationBadge } from './NotificationBadge';
